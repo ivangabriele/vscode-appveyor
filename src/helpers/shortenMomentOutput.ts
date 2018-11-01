d36d35133a20d905c9167d6f8fe9f9c474dc39e8
Ivan Gabriele <ivan.gabriele@gmail.com>
2018-11-01 23:43:47 +0100

    helpers/shortenMomentOutput: fix edge cases

--- src/helpers/shortenMomentOutput.ts
+++ src/helpers/shortenMomentOutput.ts
@@ -1,9 +1,13 @@
 export default function(momentOutput: string): string {
   return momentOutput
-    .replace(/\sdays?/, 'd')
-    .replace(/\shours?/, 'h')
-    .replace(/\sminutes?/, 'm')
-    .replace(/\smonths?/, 'mo')
-    .replace(/\sseconds?/, 's')
-    .replace(/\syears?/, 'y')
+    .replace(/a day/, '1d')
+    .replace(/ days/, 'd')
+    .replace(/an hour/, '1h')
+    .replace(/ hours/, 'h')
+    .replace(/a minute/, '1m')
+    .replace(/ minutes/, 'm')
+    .replace(/a month/, '1mo')
+    .replace(/ months/, 'mo')
+    .replace(/a year?/, '1y')
+    .replace(/ years/, 'y')
 }
